472b020d38cf6475b788fd6020e7854affa1f9c7
Fernando-IURISCI <Fernando.IURISCI@vodafone.com>
2026-09-02 09:35:25 +0200

    Revert "piccola modifica su mathes predction per non farla dipendere dai dati del torneo"
    This reverts commit bb95e95bbb7cf96d7a000cf0d460f744c051aaa5.

--- webfuction/webdata/probable/probable-match-result.vb
+++ webfuction/webdata/probable/probable-match-result.vb
@@ -6,12 +6,14 @@ Namespace WebData
     Public Class ProbableMatchResult
 
         Dim appSett As New Torneo.PublicVariables
+        Dim mdatat As Torneo.MatchsData
         Dim mdataw As MatchsData
         Dim dirTemp As String = ""
         Dim dirData As String = ""
 
         Sub New(appSett As Torneo.PublicVariables)
             Me.appSett = appSett
+            mdatat = New Torneo.MatchsData(appSett)
             mdataw = New MatchsData(appSett)
             dirTemp = appSett.WebDataPath & "temp\"
             dirData = appSett.WebDataPath & "data\pmatchs\"
@@ -24,24 +26,22 @@ Namespace WebData
             Dim fileTemp As String = dirTemp & "pronostici.txt"
             Dim fileLog As String = dirData & "pronostici.log"
             Dim str As New System.Text.StringBuilder
+            Dim matchs As List(Of Torneo.MatchsData.Match) = mdatat.GetMatchsData("-1")
+
+            Dim html As String = Functions.GetPage(appSett, "https://www.superpronostici.it/pronostici/calcio/it/serie-a", "UTF-8")
+            IO.File.WriteAllText(fileTemp, html, System.Text.Encoding.Default)
             Dim logs As New System.Text.StringBuilder
 
             Try
-
-                Dim matchs As List(Of Torneo.MatchsData.Match) = mdataw.GetMatchsData()
-
-                Dim html As String = Functions.GetPage(appSett, "https://www.superpronostici.it/pronostici/calcio/it/serie-a", "UTF-8")
-                IO.File.WriteAllText(fileTemp, html, System.Text.Encoding.Default)
 
                 logs.AppendLine("Loading matchs")
                 logs.AppendLine("Year -> " & appSett.Year)
                 logs.AppendLine("Calendario match:")
                 logs.AppendLine("---------------------------")
-                For Each t As Torneo.MatchsData.Match In matchs
-                    logs.AppendLine(t.Giornata & " -> " & t.TeamA & " vs " & t.TeamB)
+                For Each t As String In mdataw.KeyMatchs.Keys
+                    logs.AppendLine(mdataw.KeyMatchs(t).Giornata & " -> " & t)
                 Next
-                logs.AppendLine(matchs.Count.ToString())
-
+                logs.AppendLine("")
 
                 If html <> "" Then
 
@@ -53,24 +53,22 @@ Namespace WebData
                     Dim pstate As String = "Titolare"
                     Dim team As String = ""
 
-                    Dim wdata As New Dictionary(Of Integer, List(Of Torneo.ProbableMatchResult.ProbableResult))
-                    Dim giornata As Integer = -1
+                    Dim wdata As New List(Of Torneo.ProbableMatchResult.ProbableResult)
+                    Dim giornata As String = ""
                     Dim hometeam As String = ""
                     Dim awayteam As String = ""
 
                     Dim m1 As Text.RegularExpressions.MatchCollection = System.Text.RegularExpressions.Regex.Matches(IO.File.ReadAllText(fileTemp, System.Text.Encoding.Default), "(""giornata"":\d+,""matchId"":\d+,""homeTeam"":""\w+"",""awayTeam"":""\w+"")|(""prob_1"":[\d\.]{1,},""prob_x"":[\d\.]{1,},""prob_2"":[\d\.]{1,})")
-                    Dim wstart As New List(Of Integer)
-
                     For Each m As Text.RegularExpressions.Match In m1
 
                         If m.Value.Contains("matchId") Then
-                            giornata = Convert.ToInt32(System.Text.RegularExpressions.Regex.Match(m.Value, "(?<=giornata"":)\d+").Value)
+                            giornata = System.Text.RegularExpressions.Regex.Match(m.Value, "(?<=giornata"":)\d+").Value
                             Dim matchid As String = System.Text.RegularExpressions.Regex.Match(m.Value, "(?<=matchId"":)\d+").Value
                             hometeam = Functions.CheckTeamName(System.Text.RegularExpressions.Regex.Match(m.Value, "(?<=homeTeam"":)""\w+""").Value.Replace("""", "").Replace("homeTeam:", ""))
                             awayteam = Functions.CheckTeamName(System.Text.RegularExpressions.Regex.Match(m.Value, "(?<=awayTeam"":)""\w+""").Value.Replace("""", "").Replace("awayTeam:", ""))
                             logs.AppendLine("Match -> " & matchid & " - " & hometeam & " vs " & awayteam)
-                            If wstart.Contains(giornata) AndAlso matchs.Where(Function(x) x.Giornata = (giornata) AndAlso CDate(x.Time) < Date.Now).Count > 0 Then
-                                wstart.Add(giornata)
+                            If matchs.Where(Function(x) x.Giornata = Convert.ToInt32(giornata) AndAlso CDate(x.Time) < Date.Now).Count > 0 Then
+                                Return ""
                             End If
                         ElseIf m.Value.Contains("prob_1") Then
                             Dim prob1 As Integer = Convert.ToInt32(Convert.ToDouble(System.Text.RegularExpressions.Regex.Match(m.Value, "(?<=prob_1"":)[\d\.]{1,}").Value.Replace(".", Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator)))
@@ -80,20 +78,17 @@ Namespace WebData
                                 prob2 = 100 - prob1 - probx
                             End If
                             logs.AppendLine("Probabilities -> 1: " & prob1 & ", X: " & probx & ", 2: " & prob2)
-                            If wdata.ContainsKey(giornata) = False Then wdata.Add(giornata, New List(Of Torneo.ProbableMatchResult.ProbableResult))
-                            wdata(giornata).Add(New Torneo.ProbableMatchResult.ProbableResult With {.Match = hometeam & "-" & awayteam, .TeamHome = hometeam, .TeamAway = awayteam, .Prob1 = Convert.ToInt32(prob1), .ProbX = Convert.ToInt32(probx), .Prob2 = Convert.ToInt32(prob2)})
+                            wdata.Add(New Torneo.ProbableMatchResult.ProbableResult With {.Match = hometeam & "-" & awayteam, .TeamHome = hometeam, .TeamAway = awayteam, .Prob1 = Convert.ToInt32(prob1), .ProbX = Convert.ToInt32(probx), .Prob2 = Convert.ToInt32(prob2)})
 
                         End If
                     Next
 
-                    For Each g As Integer In wdata.Keys
-                        If wstart.Contains(g) = False Then
-                            Dim fileName As String = dirData & "\" & g & ".json"
-                            If IO.Directory.Exists(dirData) = False Then IO.Directory.CreateDirectory(dirData)
-                            Dim out As String = WriteData(wdata(g), fileName)
-                            rmsg = WebData.Functions.SerializzaOggetto(wdata(g), False)
-                        End If
-                    Next
+                    If giornata <> "" Then
+                        Dim fileName As String = dirData & "\" & giornata & ".json"
+                        If IO.Directory.Exists(dirData) = False Then IO.Directory.CreateDirectory(dirData)
+                        Dim out As String = WriteData(wdata, fileName)
+                        rmsg = WebData.Functions.SerializzaOggetto(wdata, False)
+                    End If
                 End If
 
             Catch ex As Exception
